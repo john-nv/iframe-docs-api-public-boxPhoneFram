d33a27c810ef31f8018bc78caa011bc837b44771
quang-kingfood <quang.ngovan@kingfoodmart.net>
2023-12-14 18:53:05 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,70 @@
+const apiUrl = 'http://localhost:5001/api';
+const localStorageKey = 'token';
+let token = localStorage.getItem(localStorageKey);
+
+$(document).ready(async function() {
+    $("#domain").text(apiUrl);
+
+    if (!token || token.length < 1) {
+        showDialogAlert();
+    }
+
+    await getKey();
+
+    $('#login').on('click', async () => {
+        const username = $('#username').val();
+        const password = $('#password').val();
+
+        if (username.length < 3 || password.length < 4) {
+            return alert('Please enter your email and password');
+        }
+
+        await login(username, password);
+    });
+});
+
+async function login(email, password) {
+    try {
+        const response = await $.ajax({
+            url: `${apiUrl}/v1/token`,
+            type: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify({ email, password }),
+        });
+
+        token = response.token;
+        localStorage.setItem(localStorageKey, token);
+        await getKey();
+        hideDialogAlert();
+    } catch (error) {
+        alert(error.responseJSON.message);
+        clearInputFields();
+    }
+}
+
+async function getKey() {
+    try {
+        const response = await $.ajax({
+            url: `${apiUrl}/v1/token`,
+            type: 'GET',
+            headers: { 'Authorization': `Bearer ${token}` },
+        });
+
+        $('#key').text(response.token);
+    } catch (error) {
+        showDialogAlert();
+    }
+}
+
+function showDialogAlert() {
+    $('#dialog_alert').modal('show');
+}
+
+function hideDialogAlert() {
+    $('#dialog_alert').modal('hide');
+}
+
+function clearInputFields() {
+    $('#username').val('');
+    $('#password').val('');
+}
